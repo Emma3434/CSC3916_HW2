e8ea4a339ac38eb881ff07c3d34338087a815e55
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-02-26 13:58:40 -0700

    changed basic authorization from hard code to finding user

--- auth.js
+++ auth.js
@@ -5,8 +5,10 @@ var BasicStrategy = require('passport-http').BasicStrategy;
 passport.use(new BasicStrategy(
     function(username, password, done) {
         //hard coded
-        var user = { name: "testuser" };
-        if (username == user.name && password == "cu")
+        //var user = { name: "testuser" };
+        var user = db.findOne(req.body.username)
+        //if (username == user.name && password == "cu")
+        if(req.body.password == user.password)
         {
             return done(null, user);
         }
