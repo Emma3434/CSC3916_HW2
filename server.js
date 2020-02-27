d9e68d2493607200828e5ff0edfbfeff7299d026
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-02-27 16:07:07 -0700

    query added

--- server.js
+++ server.js
@@ -28,6 +28,9 @@ function getJSONObject(req) {
     if (req.headers != null) {
         json.headers = req.headers;
     }
+    if (req.query != null) {
+        json.query = req.query;
+    }
 
     return json;
 }
