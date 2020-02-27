f53014e1b79ba478aff9b93ca6bbbde94dceaeb4
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-02-27 15:34:30 -0700

    changed back to just hardcoded basic auth

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
 
-/*passport.use(new BasicStrategy(
+passport.use(new BasicStrategy(
     function(username, password, done) {
         //hard coded
         var user = { name: "testuser" };
@@ -14,49 +14,6 @@ var BasicStrategy = require('passport-http').BasicStrategy;
         {
             return done(null, false);
         }
-    }
-));*/
-
-console.log(passport);
-console.log(BasicStrategy);
-
-passport.use(new BasicStrategy(
-    function(username, password, done) {
-        var user = db.findOne(username)
-
-        if (user)
-        {
-            done(null,false);
-        }
-        else{
-            if (password == user.password) {
-                done(null, user)
-            }
-        }
-        /*
-        var user = db.findOne(req.body.username);
-
-        if (!user) {
-            res.status(401).send({success: false, msg: 'Authentication failed. User not found.'});
-        }
-        else {
-            // check if password matches
-            if (req.body.password == user.password)  {
-                var userToken = { id : user.id, username: user.username };
-                var token = jwt.sign(userToken, process.env.SECRET_KEY);
-                res.json({success: true, token: 'JWT ' + token});
-            }
-            else {
-                res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
-            }
-        };*/
-        /*
-        db.findOne({username: req.body.username}, function (err, user) {
-            if (err) { return done(err); }
-            if (!user) { return done(null, false); }
-            if (!user.validPassword(password)) { return done(null, false); }
-            return done(null, user);
-        });*/
     }
 ));
 exports.isAuthenticated = passport.authenticate('basic', { session : false });
